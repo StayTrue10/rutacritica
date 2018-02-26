9153c08057560291d9799f8240b5b06839e1a29a
Kabir Alberto <35808948+StayTrue10@users.noreply.github.com>
2018-02-26 15:11:18 -0500

    Final
    combobox, hora, validacion de registro (existencia), campos vacios. 
    bd = string (todos los campos).

---
+++ ConsultaSQL.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace RUTACRITICA
+{
+    class ConsultaSQL
+    {
+        private SqlConnection conexion = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = RUTACRITICA; user id=sa; password=Pr0c3s0.12");
+
+        public bool Salidas(string ticket, string id, string fecha, string hora)
+        {
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand(string.Format("Insert into Salidas values ('{0}', '{1}', '{2}', '{3}')", new string[] { ticket, id, fecha, hora }), conexion);
+            int filasafectadas = cmd.ExecuteNonQuery();
+            conexion.Close();
+            if (filasafectadas > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool Entradas(string ticket, string id, string fecha, string hora)
+        {
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand(string.Format("insert into Entradas values ('{0}', '{1}', '{2}', '{3}')", new string[] { ticket, id, fecha, hora }), conexion);
+            int filasafectadas = cmd.ExecuteNonQuery();
+            conexion.Close();
+            if (filasafectadas > 0) return true;
+            else return false;
+        }
+
+        public void idsalida(ComboBox cbID1)
+         {
+             cbID1.Items.Clear();
+             conexion.Open();
+             SqlCommand cmd = new SqlCommand("Select * from Diligencieros", conexion);
+             SqlDataReader dr = cmd.ExecuteReader();
+             while (dr.Read())
+             {
+                 cbID1.Items.Add(dr[0].ToString());
+             }
+             conexion.Close();
+             cbID1.Items.Insert(0, "");
+             cbID1.SelectedIndex = 0;
+         }
+        public void identrada(ComboBox cbID1)
+        {
+            cbID1.Items.Clear();
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Diligencieros", conexion);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                cbID1.Items.Add(dr[0].ToString());
+            }
+            conexion.Close();
+            cbID1.Items.Insert(0, "");
+            cbID1.SelectedIndex = 0;
+        }
+
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RUTACRITICA
+{
+    public partial class Form1 : Form
+    {
+        ConsultaSQL sql = new ConsultaSQL();
+        public Form1()
+        {
+            InitializeComponent();
+            sql.idsalida(cbID1);
+            sql.identrada(cbID2);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tbFecha1.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            tbHora1.Text = DateTime.Now.ToString("hh:mm:ss");
+            tbFecha2.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            tbHora2.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+        
+        private void btnRegistrar1_Click(object sender, EventArgs e)
+        {
+            if (cbID1.Text.Length == 0 || tbTicket1.Text.Length == 0)
+            {
+                MessageBox.Show("Faltan campos por rellenar", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                using (SqlConnection cn = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = RUTACRITICA; user id=sa; password=Pr0c3s0.12"))
+                {
+                    var cmd = new SqlCommand("SELECT COUNT(*) FROM Salidas WHERE ticket = @ticket", cn);
+                    cmd.Parameters.AddWithValue("@ticket", tbTicket1.Text);
+                    cn.Open();
+                    if (Convert.ToInt32(cmd.ExecuteScalar()) == 0)
+                    {
+                        if (sql.Salidas(tbTicket1.Text, cbID1.Text, tbFecha1.Text, tbHora1.Text))
+                        {
+                            MessageBox.Show("Registrado con exito!", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudo registrar", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Este ticket ya esta registrado", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+            }
+        }
+       private void btnRegistrar2_Click(object sender, EventArgs e)
+        {
+            if (cbID2.Text.Length == 0 || tbTicket2.Text.Length == 0)
+            {
+                MessageBox.Show("Faltan campos por rellenar", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                using (SqlConnection cn = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = RUTACRITICA; user id=sa; password=Pr0c3s0.12"))
+                {
+                    var cmd = new SqlCommand("SELECT COUNT(*) FROM Entradas WHERE ticket = @ticket", cn);
+                    cmd.Parameters.AddWithValue("@ticket", tbTicket2.Text);
+                    cn.Open();
+                    if (Convert.ToInt32(cmd.ExecuteScalar()) == 0)
+                    {
+                        if (sql.Entradas(tbTicket2.Text, cbID2.Text, tbFecha2.Text, tbHora2.Text))
+                        {
+                            MessageBox.Show("Registrado con exito!", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudo registrar", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Este ticket ya esta registrado", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+            }
+        }
+        private void btnNuevo1_Click(object sender, EventArgs e)
+        {
+            cbID1.Text = "";
+            tbTicket1.Text = "";
+        }
+        private void btnNuevo2_Click(object sender, EventArgs e)
+        {
+            cbID2.Text = "";
+            tbTicket2.Text = "";
+        }
+       
+
+
+    }
+}
