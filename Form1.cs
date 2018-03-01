48e16f9921b72bee96001dab3576176e84785da2
Kabir Alberto <35808948+StayTrue10@users.noreply.github.com>
2018-03-01 15:33:50 -0500

    Final
    conexion, hora actual, registro de salidas y entradas; no campos vacios, verifica que exista id, que no se repitan tickets y limpia los campos, boton nuevo; limpia campos.

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,17 +17,14 @@ namespace RUTACRITICA
         public Form1()
         {
             InitializeComponent();
-            sql.idsalida(cbID1);
-            sql.identrada(cbID2);
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
             tbFecha1.Text = DateTime.Now.ToString("dd/MM/yyyy");
-            tbHora1.Text = DateTime.Now.ToString("hh:mm:ss");
+            tbHora1.Text = DateTime.Now.ToString("HH:mm:ss");
             tbFecha2.Text = DateTime.Now.ToString("dd/MM/yyyy");
-            tbHora2.Text = DateTime.Now.ToString("hh:mm:ss");
+            tbHora2.Text = DateTime.Now.ToString("HH:mm:ss");
         }
-        
         private void btnRegistrar1_Click(object sender, EventArgs e)
         {
             if (cbID1.Text.Length == 0 || tbTicket1.Text.Length == 0)
@@ -40,26 +37,37 @@ namespace RUTACRITICA
                 {
                     var cmd = new SqlCommand("SELECT COUNT(*) FROM Salidas WHERE ticket = @ticket", cn);
                     cmd.Parameters.AddWithValue("@ticket", tbTicket1.Text);
+                    var control = new SqlCommand("SELECT COUNT(*) FROM Diligencieros WHERE id = @id", cn);
+                    control.Parameters.AddWithValue("@id", cbID1.Text);
                     cn.Open();
-                    if (Convert.ToInt32(cmd.ExecuteScalar()) == 0)
+                    if (Convert.ToInt32(control.ExecuteScalar()) == 0)
                     {
-                        if (sql.Salidas(tbTicket1.Text, cbID1.Text, tbFecha1.Text, tbHora1.Text))
-                        {
-                            MessageBox.Show("Registrado con exito!", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
-                        else
-                        {
-                            MessageBox.Show("No se pudo registrar", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                        }
+                        MessageBox.Show("El ID no exite", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     }
                     else
                     {
-                        MessageBox.Show("Este ticket ya esta registrado", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        if (Convert.ToInt32(cmd.ExecuteScalar()) == 0)
+                        {
+                            if (sql.Salidas(tbTicket1.Text, cbID1.Text, tbFecha1.Text, tbHora1.Text))
+                            {
+                                MessageBox.Show("Registrado con exito!", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show("No se pudo registrar", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Este ticket ya esta registrado", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
                     }
                 }
             }
+            cbID1.Text = "";
+            tbTicket1.Text = "";
         }
-       private void btnRegistrar2_Click(object sender, EventArgs e)
+        private void btnRegistrar2_Click(object sender, EventArgs e)
         {
             if (cbID2.Text.Length == 0 || tbTicket2.Text.Length == 0)
             {
@@ -71,24 +79,35 @@ namespace RUTACRITICA
                 {
                     var cmd = new SqlCommand("SELECT COUNT(*) FROM Entradas WHERE ticket = @ticket", cn);
                     cmd.Parameters.AddWithValue("@ticket", tbTicket2.Text);
+                    var control = new SqlCommand("SELECT COUNT(*) FROM Diligencieros WHERE id = @id", cn);
+                    control.Parameters.AddWithValue("@id", cbID2.Text);
                     cn.Open();
-                    if (Convert.ToInt32(cmd.ExecuteScalar()) == 0)
+                    if (Convert.ToInt32(control.ExecuteScalar()) == 0)
                     {
-                        if (sql.Entradas(tbTicket2.Text, cbID2.Text, tbFecha2.Text, tbHora2.Text))
-                        {
-                            MessageBox.Show("Registrado con exito!", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
-                        else
-                        {
-                            MessageBox.Show("No se pudo registrar", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                        }
+                        MessageBox.Show("El ID no exite", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     }
                     else
                     {
-                        MessageBox.Show("Este ticket ya esta registrado", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        if (Convert.ToInt32(cmd.ExecuteScalar()) == 0)
+                        {
+                            if (sql.Entradas(tbTicket2.Text, cbID2.Text, tbFecha2.Text, tbHora2.Text))
+                            {
+                                MessageBox.Show("Registrado con exito!", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show("No se pudo registrar", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Este ticket ya esta registrado", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
                     }
                 }
             }
+            cbID2.Text = "";
+            tbTicket2.Text = "";
         }
         private void btnNuevo1_Click(object sender, EventArgs e)
         {
@@ -100,8 +119,5 @@ namespace RUTACRITICA
             cbID2.Text = "";
             tbTicket2.Text = "";
         }
-       
-
-
     }
 }
