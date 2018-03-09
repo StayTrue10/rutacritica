4ee479540f96cedc955171b8d588db10bb9932ca
Kabir Alberto <35808948+StayTrue10@users.noreply.github.com>
2018-03-09 17:55:55 -0500

    Final
    SQL los datos son correctos

--- ConsultaSQL.cs
+++ ConsultaSQL.cs
@@ -11,25 +11,29 @@ namespace RUTACRITICA
 {
     class ConsultaSQL
     {
+        //Conexion a sql server
         private SqlConnection conexion = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = RUTACRITICA; user id=sa; password=Pr0c3s0.12");
-        public bool Salidas(string ticket, string id, string fecha, string hora)
+        //Insert datos a bd (varchar, int, date, time)
+        public bool Salidas(string ticket, int id, string fecha, string hora)
         {
+            //Abre la conexion e inserta en las columas correspondientes
             conexion.Open();
-            SqlCommand cmd = new SqlCommand(string.Format("Insert into Salidas values ('{0}', '{1}', '{2}', '{3}')", new string[] { ticket, id, fecha, hora }), conexion);
-            int filasafectadas = cmd.ExecuteNonQuery();
+            SqlCommand cmd = new SqlCommand(string.Format("Insert into Salidas values ('{0}', {1}, '{2}', '{3}')", ticket, id, fecha, hora), conexion);
+            //Filas afectadas mayores a 0, registradas con exito
+            int n = cmd.ExecuteNonQuery();
             conexion.Close();
-            if (filasafectadas > 0)
+            if (n > 0)
                 return true;
             else
                 return false;
         }
-        public bool Entradas(string ticket, string id, string fecha, string hora)
+        public bool Entradas(string ticket, int id, string fecha, string hora)
         {
             conexion.Open();
-            SqlCommand cmd = new SqlCommand(string.Format("insert into Entradas values ('{0}', '{1}', '{2}', '{3}')", new string[] { ticket, id, fecha, hora }), conexion);
-            int filasafectadas = cmd.ExecuteNonQuery();
+            SqlCommand cmd = new SqlCommand(string.Format("Insert into Entradas values ('{0}', {1}, '{2}', '{3}')", ticket, id, fecha, hora), conexion);
+            int n = cmd.ExecuteNonQuery();
             conexion.Close();
-            if (filasafectadas > 0)
+            if (n > 0)
                 return true;
             else
                 return false;
